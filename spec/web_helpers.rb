a61918ac78ea47229c1218d6dc7bb8f457b151da
Joe McGarry <joewmcgarry@gmail.com>
2022-06-14 17:09:24 +0100

    adds coverage json files to .gitignore

--- spec/web_helpers.rb
+++
@@ -1,7 +0,0 @@
-def user_sign_up
-  fill_in :first_name, with: "Iain"
-  fill_in :last_name, with: "Banks"
-  fill_in :email, with: "ian@test.com"
-  fill_in :password, with: "password123"
-  click_button "Sign up"
-end
