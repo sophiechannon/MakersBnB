51fa4e7c9216f93df911eb0a20a2b14d884dfef8
Michael Chan <michaelcychan@Michaels-MacBook-Air.local>
2022-06-14 11:00:45 +0100

    property class .all retrieving data from the  database

--- lib/property.rb
+++ lib/property.rb
@@ -1,9 +1,17 @@
+require 'pg'
+
 class Property
   def self.all
-    [
-      "A haunted house by the sea",
-      "Property 2",
-      "Property 3",
-    ]
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test')
+    else
+      connection = PG.connect(dbname: 'makersbnb')
+    end
+    result = connection.exec_params(
+      "SELECT * from properties", []
+    )
+    result.map{ |entry|
+      entry['property_name']
+    }
   end
 end
--- spec/features/viewing_properties_spec.rb
+++ spec/features/viewing_properties_spec.rb
@@ -2,9 +2,19 @@ require "pg"
 
 feature "viewing properties" do
   scenario "viewing properties on /spaces route" do
+    connection = PG.connect(dbname: 'makersbnb_test')
+    connection.exec_params(
+      "INSERT INTO properties (property_name) VALUES($1)",
+      ['A haunted house by the sea']
+    )
+    connection.exec_params(
+      "INSERT INTO properties (property_name) VALUES($1)",
+      ['A house in the moutains']
+    )
+    
     visit("/spaces")
     expect(page).to have_content "Book a space"
     expect(page).to have_content "A haunted house by the sea"
-    expect(page).to have_content "Property 2"
+    expect(page).to have_content "A house in the moutains"
   end
 end
