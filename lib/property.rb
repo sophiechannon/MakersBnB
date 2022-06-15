1aec4a62fac3997e1c94d88d826d852db7d3ec96
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-06-15 15:54:47 +0100

    request to book feature test working with text hard coded into post route

--- lib/property.rb
+++ lib/property.rb
@@ -1,9 +1,10 @@
 require "pg"
 
 class Property
-  attr_reader :name, :description, :price, :user_id
+  attr_reader :id, :name, :description, :price, :user_id
 
-  def initialize(name:, description:, price:, user_id:)
+  def initialize(id:, name:, description:, price:, user_id:)
+    @id = id
     @name = name
     @description = description
     @price = price
@@ -20,7 +21,7 @@ class Property
       "SELECT * from properties", []
     )
     result.map { |entry|
-      Property.new(name: entry["property_name"], description: entry["property_description"], price: entry["price_per_night"], user_id: entry["user_id"])
+      Property.new(id: entry["id"], name: entry["property_name"], description: entry["property_description"], price: entry["price_per_night"], user_id: entry["user_id"])
     }
   end
 
@@ -31,9 +32,29 @@ class Property
       @connection = PG.connect(dbname: "makersbnb")
     end
     result = @connection.exec_params(
-      "INSERT INTO properties (property_name, property_description, price_per_night, user_id) VALUES($1, $2, $3, $4) RETURNING property_name, property_description, price_per_night, user_id",
+      "INSERT INTO properties (property_name, property_description, price_per_night, user_id) VALUES($1, $2, $3, $4) RETURNING id, property_name, property_description, price_per_night, user_id",
       [name, description, price, user_id]
     )
-    Property.new(name: result[0]["property_name"], description: result[0]["property_description"], price: result[0]["price_per_night"], user_id: result[0]["user_id"])
+    Property.new(id:result[0]["id"], name: result[0]["property_name"], description: result[0]["property_description"], price: result[0]["price_per_night"], user_id: result[0]["user_id"])
+  end
+
+  def self.find(id:)
+    return nil if id == nil
+    if ENV['ENVIRONMENT'] == 'test'
+      @connection = PG.connect(dbname: 'makersbnb_test')
+    else
+      @connection = PG.connect(dbname: 'makersbnb')
+    end
+    result = @connection.exec_params(
+      "SELECT * FROM properties WHERE id = $1",
+      [id]
+    )
+    Property.new(
+      id:result[0]["id"], 
+      name: result[0]["property_name"], 
+      description: result[0]["property_description"], 
+      price: result[0]["price_per_night"], 
+      user_id: result[0]["user_id"]
+    )
   end
 end
--- spec/property_spec.rb
+++ spec/property_spec.rb
@@ -15,6 +15,7 @@ describe Property do
       expect(result.first.name).to eq "A haunted house by the sea"
       expect(result.first.description).to eq "It is an old haunted house, you will definitely love the fear."
       expect(result.first.user_id).to eq user.id
+      expect(result.first.id).to eq result[0].id
     end
   end
 
@@ -27,6 +28,22 @@ describe Property do
       expect(Property.all.first.description).to eq "It is an old haunted house, you will definitely love the fear."
       expect(Property.all.first.price).to eq "25"
       expect(Property.all.first.user_id).to eq user.id
+    end
+  end
+
+  describe '.find' do
+    it 'finds the property with the property id' do
+      user = User.create(first_name: "Iain", last_name: "Banks", email: "iain@test.com", password: "password123")
+      property = Property.create(name: "A haunted house by the sea", description: "It is an old haunted house, you will definitely love the fear.",
+        price: "25", user_id: user.id)
+      result = Property.find(id: property.id)
+      expect(result.name).to eq "A haunted house by the sea"
+      expect(result.description).to eq "It is an old haunted house, you will definitely love the fear."
+      expect(result.price).to eq "25"
+    end
+
+    it 'returns nil if no user is found' do
+      expect(User.find(id: nil)).to eq nil
     end
   end
 end
