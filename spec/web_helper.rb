0d1b68f5ef42e020211036d965d07982893bd7d2
Joe McGarry <joewmcgarry@gmail.com>
2022-06-15 11:35:24 +0100

    Adds web helper for user logging in

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,4 +1,3 @@
-
 def list_a_property(name:, description:, price:)
   visit("/spaces/new")
   fill_in("property_name", with: name)
@@ -13,4 +12,13 @@ def user_sign_up
   fill_in :email, with: "ian@test.com"
   fill_in :password, with: "password123"
   click_button "Sign up"
+end
+
+def user_log_in
+  User.create(first_name: "Ian", last_name: "Banks", email: "iain@test.com", password: "password123")
+  visit "/"
+  click_link "Log in"
+  fill_in :email, with: "iain@test.com"
+  fill_in :password, with: "password123"
+  click_button "Log in"
 end
