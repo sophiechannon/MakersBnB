d8df27db2231eeede706183bc8bffaa8ba424acb
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-06-16 17:06:08 +0100

    all css that we can do is done

--- spec/features/user_can_request_to_book_spec.rb
+++ spec/features/user_can_request_to_book_spec.rb
@@ -4,7 +4,7 @@ feature "booking a space" do
     property = Property.create(name: "A house in the moutains", description: "A big house in the mountains. You will enjoy the views and the songs of the birds.",
                                price: "50", user_id: user.id, availability_start: "2022-05-10", availability_end: "2022-10-20")
     user_log_in
-    first(".listing").click_button "Request to book"
+    first(".listing").click_link "listing"
     fill_in :date, with: "2022-07-01"
     click_button "Request"
     expect(page).to have_content "Your request to book #{property.name} has been submitted"
