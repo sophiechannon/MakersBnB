1e93d0556637ea1b9f62658f014ecb80eb57776f
Christopher <christopher2brown@hotmail.co.uk>
2022-06-16 12:02:32 +0100

    fixing bugs

--- spec/features/user_can_request_to_book_spec.rb
+++ spec/features/user_can_request_to_book_spec.rb
@@ -1,10 +1,8 @@
 feature "booking a space" do
   scenario "user sucessfully books a space for one night" do
-    user = User.create(first_name: "Ian", last_name: "Banks", email: "iain@test.com", password: "password123")
-    property = Property.create(name: "A haunted house by the sea", description: "It is an old haunted house, you will definitely love the fear.",
-      price: "25", user_id: user.id)
+   user = User.create(first_name: "Ian", last_name: "Banks", email: "iain@test.com", password: "password123")
     Property.create(name: "A house in the moutains", description: "A big house in the mountains. You will enjoy the views and the songs of the birds.",
-      price: "50", user_id: user.id)
+      price: "50", user_id: user.id, availability_start: "2022-05-10", availability_end: "2022-10-20")
     user_log_in
     first('.listing').click_button 'Request to book'
     fill_in :date, with: '17/06/2022'
