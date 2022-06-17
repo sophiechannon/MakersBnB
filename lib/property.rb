33b36fe340f7859d58c40447098091e60eb9ea90
Joe McGarry <joewmcgarry@gmail.com>
2022-06-17 11:45:10 +0100

    Shortens description on /spaces if above 60 characters

--- lib/property.rb
+++ lib/property.rb
@@ -1,11 +1,9 @@
 require "pg"
 
 class Property
-
   attr_reader :name, :description, :price, :user_id, :availability_start, :availability_end, :id
 
   def initialize(name:, description:, price:, user_id:, availability_start:, availability_end:, id:)
-
     @name = name
     @description = description
     @price = price
@@ -25,7 +23,6 @@ class Property
       "SELECT * from properties", []
     )
     result.map { |entry|
-
       Property.new(name: entry["property_name"], description: entry["property_description"], price: entry["price_per_night"], user_id: entry["user_id"], availability_start: entry["availability_start_date"], availability_end: entry["availability_end_date"], id: entry["id"])
     }
   end
@@ -37,7 +34,7 @@ class Property
       @connection = PG.connect(dbname: "makersbnb")
     end
     result = @connection.exec_params(
-      "INSERT INTO properties (property_name, property_description, price_per_night, user_id, availability_start_date, availability_end_date) VALUES($1, $2, $3, $4, $5, $6) RETURNING property_name, property_description, price_per_night, user_id, availability_start_date, availability_end_date, id", 
+      "INSERT INTO properties (property_name, property_description, price_per_night, user_id, availability_start_date, availability_end_date) VALUES($1, $2, $3, $4, $5, $6) RETURNING property_name, property_description, price_per_night, user_id, availability_start_date, availability_end_date, id",
       [name, description, price, user_id, availability_start, availability_end]
     )
     Property.new(name: result[0]["property_name"], description: result[0]["property_description"], price: result[0]["price_per_night"], user_id: result[0]["user_id"], availability_start: result[0]["availability_start_date:"], availability_end: result[0]["availability_end_date"], id: result[0]["id"])
@@ -45,23 +42,23 @@ class Property
 
   def self.find(id:)
     return nil if id == nil
-    if ENV['ENVIRONMENT'] == 'test'
-      @connection = PG.connect(dbname: 'makersbnb_test')
+    if ENV["ENVIRONMENT"] == "test"
+      @connection = PG.connect(dbname: "makersbnb_test")
     else
-      @connection = PG.connect(dbname: 'makersbnb')
+      @connection = PG.connect(dbname: "makersbnb")
     end
     result = @connection.exec_params(
       "SELECT * FROM properties WHERE id = $1",
       [id]
     )
     Property.new(
-      id:result[0]["id"], 
-      name: result[0]["property_name"], 
-      description: result[0]["property_description"], 
-      price: result[0]["price_per_night"], 
+      id: result[0]["id"],
+      name: result[0]["property_name"],
+      description: result[0]["property_description"],
+      price: result[0]["price_per_night"],
       user_id: result[0]["user_id"],
       availability_start: result[0]["availability_start_date"],
-      availability_end: result[0]["availability_end_date"]
+      availability_end: result[0]["availability_end_date"],
     )
   end
 end
--- spec/features/viewing_properties_spec.rb
+++ spec/features/viewing_properties_spec.rb
@@ -3,26 +3,32 @@ require "pg"
 feature "viewing properties" do
   scenario "viewing properties on /spaces route" do
     user_log_in
-    save_and_open_page
-    list_a_property(name: "A haunted house by the sea", description: "It is an old haunted house, you will definitely love the fear. Enjoy the night!", price: "50", availability_start: "2022-06-14", availability_end: "2022-06-30" )
-    list_a_property(name: "A house in the moutains", description: "A big house in the mountains. You will enjoy the views and the songs of the birds.", price: "100",availability_start: "2022-06-18", availability_end: "2022-06-28")
+    list_a_property(name: "A haunted house by the sea", description: "It is an old haunted house, you will definitely love the fear. Enjoy the night!", price: "50", availability_start: "2022-06-14", availability_end: "2022-06-30")
+    list_a_property(name: "A house in the moutains", description: "A big house in the mountains. You will enjoy the views and the songs of the birds.", price: "100", availability_start: "2022-06-18", availability_end: "2022-06-28")
 
     expect(page).to have_content "Book a space"
     expect(page).to have_content "A haunted house by the sea"
-    expect(page).to have_content "It is an old haunted house, you will definitely love the fear. Enjoy the night!"
+    expect(page).to have_content "It is an old haunted house, you will definitely love the fear..."
     expect(page).to have_content "£50"
     expect(page).to have_content "iain@test.com"
     expect(page).to have_content "from:2022-06-14"
     expect(page).to have_content "until:2022-06-30"
     expect(page).to have_content "A house in the moutains"
-    expect(page).to have_content "A big house in the mountains. You will enjoy the views and the songs of the birds."
+    expect(page).to have_content "A big house in the mountains. You will enjoy the views and th..."
     expect(page).to have_content "£100"
   end
 
   scenario "If a user trys to enter a end date that is before start date" do
     user_log_in
-    list_a_property(name: "A haunted house by the sea", description: "It is an old haunted house, you will definitely love the fear. Enjoy the night!", price: "50", availability_start: "2022-06-14", availability_end: "2022-06-12" )
-    expect(current_path).to eq "/spaces/new" 
+    list_a_property(name: "A haunted house by the sea", description: "It is an old haunted house, you will definitely love the fear. Enjoy the night!", price: "50", availability_start: "2022-06-14", availability_end: "2022-06-12")
+    expect(current_path).to eq "/spaces/new"
     expect(page).to have_content "End date is before start date"
+  end
+
+  scenario "A long description is entered, should display a shortened description with ..." do
+    user_log_in
+    list_a_property(name: "A haunted house by the sea", description: "It is an old haunted house, you will definitely love the fear " * 50, price: "50", availability_start: "2022-06-14", availability_end: "2022-06-15")
+    expect(page).to have_content ("It is an old haunted house, you will definitely love the fear...")
+    expect(page).not_to have_content ("It is an old haunted house, you will definitely love the fear. Enjoy the night!" * 50)
   end
 end
